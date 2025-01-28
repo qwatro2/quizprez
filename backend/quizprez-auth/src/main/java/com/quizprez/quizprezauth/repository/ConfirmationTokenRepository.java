f2c927e42a1d2c392f82779d9bad32ae30ac260f
qwatro2 <kv22828@gmail.com>
2025-01-28 04:07:03 +0300

    feat: add ConfirmationTokenRepository
    Add ConfirmationTokenRepository interface implements JpaRepository for CRUD operations with ConfirmationToken entity

---
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/repository/ConfirmationTokenRepository.java
@@ -0,0 +1,10 @@
+package com.quizprez.quizprezauth.repository;
+
+import com.quizprez.quizprezauth.entity.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+    Optional<ConfirmationToken> findByToken(String token);
+}
