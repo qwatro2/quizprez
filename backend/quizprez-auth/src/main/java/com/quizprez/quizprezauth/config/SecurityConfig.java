cffc79268c434ec7c9bfb958666428b5149ca00f
qwatro2 <kv22828@gmail.com>
2025-01-28 04:06:30 +0300

    feat: add SecurityConfig
    Add SecurityConfig to configure access rights

---
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.quizprez.quizprezauth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/auth/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth -> oauth
+                    .userInfoEndpoint(userInfo -> userInfo
+                            .userService(oAuth2UserService)
+                    )
+                    .defaultSuccessUrl("/api/auth/success")
+            );
+
+        return http.build();
+    }
+}
