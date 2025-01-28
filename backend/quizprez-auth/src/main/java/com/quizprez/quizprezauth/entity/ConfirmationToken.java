08211ed8ad002912c2af96d7f7d46fd7d34ec996
qwatro2 <kv22828@gmail.com>
2025-01-28 04:01:12 +0300

    feat: add ConfirmationToken entity
    Add ConfirmationToken entity for database

---
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/entity/ConfirmationToken.java
@@ -0,0 +1,41 @@
+package com.quizprez.quizprezauth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConfirmationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public ConfirmationToken(User user) {
+        this.user = user;
+        this.token = UUID.randomUUID().toString();
+        this.expiresAt = LocalDateTime.now().plusMinutes(15); // Токен действует 15 минут
+    }
+}
