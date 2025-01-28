d3decb1fdf29c9a99cd52eefaffd6ca99eeecbda
qwatro2 <kv22828@gmail.com>
2025-01-28 03:42:02 +0300

    feat: add project default files
    Add auto-created files

---
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/QuizprezAuthApplication.java
@@ -0,0 +1,13 @@
+package com.quizprez.quizprezauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizprezAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizprezAuthApplication.class, args);
+    }
+
+}
