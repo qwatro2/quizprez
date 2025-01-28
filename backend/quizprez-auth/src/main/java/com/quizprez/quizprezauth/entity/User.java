67610f7f72bc962e489a7662ad3ef17bf40ffad6
qwatro2 <kv22828@gmail.com>
2025-01-28 04:00:42 +0300

    feat: add User entity
    Add User entity for database

---
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/entity/User.java
@@ -0,0 +1,31 @@
+package com.quizprez.quizprezauth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private boolean enabled;
+
+    private String googleId; // Для OAuth2
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
