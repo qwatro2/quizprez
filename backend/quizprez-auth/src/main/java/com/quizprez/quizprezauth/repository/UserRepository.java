5f5b59ee7f0a0f556a1c6a62bc763a97f84cb33c
qwatro2 <kv22828@gmail.com>
2025-01-28 04:04:35 +0300

    feat: add UserRepository
    Add UserRepository interface implements JpaRepository for CRUD operations with User entity

---
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.quizprez.quizprezauth.repository;
+
+import com.quizprez.quizprezauth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByGoogleId(String googleId);
+}
