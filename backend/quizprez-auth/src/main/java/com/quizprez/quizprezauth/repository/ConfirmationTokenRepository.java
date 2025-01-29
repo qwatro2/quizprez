6698bf381b7a681cbebe7a51f23037781c7c46ba
qwatro2 <kv22828@gmail.com>
2025-01-29 23:06:06 +0300

    fix: clean expired tokens
    Make cleanExpiredTokens transactional

--- backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/repository/ConfirmationTokenRepository.java
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/repository/ConfirmationTokenRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Set;
 
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
     Optional<ConfirmationToken> findByToken(String token);
-    void deleteAllByExpiresAtBefore(LocalDateTime localDateTime);
+    Set<ConfirmationToken> findAllByExpiresAtLessThan(LocalDateTime localDateTime);
 }
--- backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/service/ConfirmationTokenService.java
+++ backend/quizprez-auth/src/main/java/com/quizprez/quizprezauth/service/ConfirmationTokenService.java
@@ -1,5 +1,6 @@
 package com.quizprez.quizprezauth.service;
 
+import com.quizprez.quizprezauth.entity.ConfirmationToken;
 import com.quizprez.quizprezauth.repository.ConfirmationTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -7,8 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 
 @Service
@@ -19,9 +22,16 @@ public class ConfirmationTokenService {
 
     @Async
     @Scheduled(fixedDelay = 15 * 60 * 1000)
+    @Transactional
     public void cleanExpiredTokens() {
         logger.info("Start cleaning expired tokens");
-        tokenRepository.deleteAllByExpiresAtBefore(LocalDateTime.now());
-        logger.info("Expired tokens was cleaned");
+        Set<ConfirmationToken> expiredTokens = tokenRepository.findAllByExpiresAtLessThan(LocalDateTime.now());
+
+        if (!expiredTokens.isEmpty()) {
+            tokenRepository.deleteAll(expiredTokens);
+            logger.info("Expired tokens were cleaned successfully");
+        } else {
+            logger.info("No expired tokens found");
+        }
     }
 }
