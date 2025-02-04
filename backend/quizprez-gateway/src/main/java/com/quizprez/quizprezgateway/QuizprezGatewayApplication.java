bb8041efbff437d878f63c83490cfb0449fb6205
qwatro2 <kv22828@gmail.com>
2025-02-05 01:37:40 +0300

    feat: add API Gateway service
    Add API Gateway service

---
+++ backend/quizprez-gateway/src/main/java/com/quizprez/quizprezgateway/QuizprezGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.quizprez.quizprezgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizprezGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizprezGatewayApplication.class, args);
+    }
+
+}
