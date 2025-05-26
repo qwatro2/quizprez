15f30ddfc383ea79ec628a8ded8f73a258dc0cf2
qwatro2 <kv22828@gmail.com>
2025-05-26 22:14:16 +0300

    fix: add custom cors filter
    Add custom global cors filter

--- backend/quizprez-pptx-parsing/src/main/java/com/quizprez/quizprezpptxparsing/configs/WebConfiguration.java
+++ backend/quizprez-pptx-parsing/src/main/java/com/quizprez/quizprezpptxparsing/configs/WebConfiguration.java
@@ -1,44 +1,20 @@
 package com.quizprez.quizprezpptxparsing.configs;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableConfigurationProperties(FrontendConfig.class)
 public class WebConfiguration {
-    private final FrontendConfig frontendConfig;
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(frontendConfig.baseUrl())); // фронтенд
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/v1/**", config);
-        return source;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(withDefaults())                       // :contentReference[oaicite:0]{index=0}
-                .csrf(AbstractHttpConfigurer::disable)               // отключаем CSRF, если нужно
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated()
