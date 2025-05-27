9c95148c23e750a9e10f9c5d2d1a22f0b642061a
Matvey Rudinskiy <brokamp@mail.ru>
2025-05-27 17:42:28 +0300

    added quiz creds page

--- frontend/src/declarations.d.ts
+++ frontend/src/declarations.d.ts
@@ -8,6 +8,11 @@ declare module '*.jpg' {
     export default content;
 }
 
+declare module '*.png' {
+    const content: string;
+    export default content;
+}
+
 declare module "*.module.css" {
     const classes: { [key: string]: string };
     export default classes;
--- frontend/src/pages/quiz/quiz-creds.tsx
+++ frontend/src/pages/quiz/quiz-creds.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import BackgroundBox from "../../components/backgroundbox/backgroundbox.tsx";
-import { Box } from "@mui/material";
+import {Box, Divider, Typography} from "@mui/material";
+import {useSearchParams} from "react-router-dom";
+import qrUrl from "../../assets/qr.jpg";
+
+interface QuizCredsPageProps {
+    code: string;
+    qrCode: string;
+}
 
 export const QuizCredsPage: React.FC = () => {
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get("code") || "DEFAULT_CODE";
+    const qrCode = searchParams.get("base64") || "";
+
+    return (
+        <QuizCredsPageContent code={code} qrCode={qrCode}/>
+    );
+};
+
+export const QuizCredsPageContent: React.FC<QuizCredsPageProps> = ({code, qrCode}) => {
     return (
         <BackgroundBox>
             <Box
@@ -22,19 +39,64 @@ export const QuizCredsPage: React.FC = () => {
                         top: "50%",
                         left: "50%",
                         transform: "translate(-50%, -50%)",
-                        width: "80%",
-                        maxWidth: "600px",
-                        bgcolor: "background.paper",
+                        width: "85%",
+                        maxWidth: "750px",
+                        height: "500px",
                         borderRadius: "16px",
+                        backgroundColor: "white",
                         boxShadow: 24,
                         p: 4,
                         display: "flex",
                         flexDirection: "row",
-                        justifyContent: "space-between"
+                        justifyContent: "space-between",
+                        gap: 4,
                     }}
                 >
-                    <Box >
+                    <Box sx={{flex: 1, display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                        <Typography variant="h4" component="h1" gutterBottom
+                                    sx={{fontWeight: 'bold', textAlign: 'center'}}>
+                            Присоединяйтесь к квизу по ссылке
+                        </Typography>
+                        <Typography variant="h5" component="p" gutterBottom sx={{
+                            textAlign: 'center',
+                            color: 'primary.main',
+                            wordBreak: 'break-all',
+                            fontSize: "2rem"
+                        }}>
+                            localhost:5432/quiz/join
+                        </Typography>
+                        <Typography variant="h4" component="p" sx={{textAlign: 'center', mt: 4}}>
+                            и вводите код
+                        </Typography>
+                        <Typography variant="h3" component="p"
+                                    sx={{textAlign: 'center', fontWeight: 'bold', color: '#098842', mt: 2}}>
+                            {code}
+                        </Typography>
+                    </Box>
 
+                    <Divider orientation="vertical" flexItem/>
+
+                    <Box sx={{
+                        flex: 1,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center"
+                    }}>
+                        <Typography variant="h5" component="p" gutterBottom sx={{textAlign: 'center', mb: 3}}>
+                            Или переходите
+                        </Typography>
+                        <Typography variant="h5" component="p" gutterBottom sx={{textAlign: 'center', mb: 3}}>
+                            по QR-коду
+                        </Typography>
+                        <Box
+                            component="img"
+                            src={qrUrl}
+                            sx={{
+                                width: "200px",
+                                height: "200px"
+                            }}
+                        />
                     </Box>
                 </Box>
             </Box>
