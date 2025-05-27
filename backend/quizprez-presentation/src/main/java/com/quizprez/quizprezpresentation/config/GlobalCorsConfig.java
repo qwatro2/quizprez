5c54e0d8c1f4355759658ce61801877bc078ac0c
qwatro2 <kv22828@gmail.com>
2025-05-27 03:05:53 +0300

    fix: permit all requests in presentation service
    Permit all requests in presentation service

---
+++ backend/quizprez-presentation/src/main/java/com/quizprez/quizprezpresentation/config/GlobalCorsConfig.java
@@ -0,0 +1,36 @@
+package com.quizprez.quizprezpresentation.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(FrontendConfig.class)
+public class GlobalCorsConfig {
+    private final FrontendConfig frontendConfig;
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(frontendConfig.baseUrl()));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
